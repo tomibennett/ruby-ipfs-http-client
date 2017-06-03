ac2a33c0e4484235b3e9b817faba8cb7bb72f95d
mahloun <thms.no@gmail.com>
2017-06-03 14:50:01 +0200

    fix(command) id: delegate the response parsing and the client call to the Ipfs module

--- lib/api/generic/id.rb
+++ lib/api/generic/id.rb
@@ -1,12 +1,13 @@
-require_relative '../../client'
-
 module Ipfs
   module Command
     class Id
       PATH = '/id'
 
       def self.make_request
-        parse_response Client.call_api method: :get, path: PATH
+        {
+          method: :get,
+          path: PATH
+        }
       end
 
       def self.parse_response response
