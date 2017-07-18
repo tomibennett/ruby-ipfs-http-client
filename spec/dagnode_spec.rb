e19d373cad2b70b4add75b2f12a9bdce695c57c9
mahloun <thms.no@gmail.com>
2017-07-18 20:43:35 +0200

    feat(dagnode): DagNode objects are now responsible for response error handling

--- spec/dagnode_spec.rb
+++ spec/dagnode_spec.rb
@@ -1,14 +1,39 @@
 require_relative '../lib/dagnode'
 
 RSpec.describe Ipfs::DagNode do
-  let(:response) { double("HTTP::Response::Body") }
-  let(:dag_node) { described_class.new response }
+  describe '.initialize' do
+    context 'when response from Ipfs\' API is leading to a directory' do
+      let(:response) { double("HTTP::Response") }
 
-  it 'is a dag node' do
-    expect(dag_node).to be_a Ipfs::DagNode
-  end
+      it 'throws an error informing that the dag node was invalid' do
+        allow(response).to receive_message_chain(:status, :code) { 500 }
+        allow(response).to receive(:body) {
+          File.read \
+                 File.join('spec', 'fixtures', 'api', 'files', 'cat_invalid_dag_node.json')
+        }
 
-  it "contains the Ipfs server's response" do
-    expect(dag_node.content).to be_a response.class
+        expect {
+          described_class.new response
+        }.to raise_error(Ipfs::Error::InvalidDagNode, "this dag node is a directory")
+        end
+    end
+
+    context 'when response from Ipfs\' API is leading to a content' do
+      before do
+        allow(response).to receive_message_chain(:status, :code) { 200 }
+        allow(response).to receive(:body) { "A content" }
+      end
+
+      let(:response) { double("HTTP::Response") }
+      let(:dag_node) { described_class.new response }
+
+      it 'instanciates a dag node' do
+        expect(dag_node).to be_a Ipfs::DagNode
+      end
+
+      it "contains the Ipfs server's response" do
+        expect(dag_node.content).to be_a response.body.class
+      end
+    end
   end
 end
