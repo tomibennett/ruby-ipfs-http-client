24fe0670ab5c84c9687ffef0b5ba968a91be468f
tbenett <tom@benett.io>
2017-11-19 16:26:30 +0100

    feat(base58): invalid number are treated like empty sequence and gives an empty byte collection

--- lib/base.rb
+++ lib/base.rb
@@ -2,5 +2,15 @@ module Ipfs
   class Base58
     ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
     BASE = ALPHABET.length
+
+    def self.decode(number)
+      valid?(number) \
+        ? to_byte_array(number)
+        : []
+    end
+
+    def self.valid?(number)
+      number.match?(/[#{ALPHABET}]+/)
+    end
   end
 end
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe Ipfs::Base58 do
   it 'has the base set to 58' do
     expect(described_class::BASE).to eq 58
   end
+
+  describe '.decode' do
+    describe 'the base 58 encoded number is passed as sequence of characters' do
+      context 'when the sequence is empty' do
+        it 'returns an empty byte collection when the sequence is empty' do
+          expect(described_class.decode '').to eq []
+        end
+      end
+
+      context 'when the sequence contains number that are not in the alphabet' do
+        it 'returns an empty byte collection' do
+          expect(described_class.decode ';)').to eq []
+        end
+      end
+    end
+  end
 end
