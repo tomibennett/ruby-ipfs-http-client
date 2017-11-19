9a8f754c6b9c94f1c31842a3aeb11fee105b1b8f
tbenett <tom@benett.io>
2017-11-19 16:36:18 +0100

    feat(base58): decode a base 58 number to a byte collection

--- lib/base.rb
+++ lib/base.rb
@@ -5,8 +5,21 @@ module Ipfs
 
     def self.decode(number)
       valid?(number) \
-        ? to_byte_array(number)
+        ? to_byte_array(to_base_10(number))
         : []
+    end
+
+    def self.to_base_10(base58_number)
+      base58_number
+        .reverse
+        .split(//)
+        .each_with_index.reduce(0) do |base10_number, (base58_numeral, index)|
+        base10_number + ALPHABET.index(base58_numeral) * (BASE**index)
+      end
+    end
+
+    def self.to_byte_array(base10_number)
+      [base10_number.to_s(16)].pack('H*').unpack('C*')
     end
 
     def self.valid?(number)
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -22,6 +22,18 @@ RSpec.describe Ipfs::Base58 do
           expect(described_class.decode ';)').to eq []
         end
       end
+
+      context 'when the sequence is a valid base 58 number' do
+        let(:number) { 'QmfZw5U6oB54PgrbNv7QF314As1VaA4NUhxCAD92SAdq45' }
+        let(:byte_array) {
+          [18, 32, 255, 253, 188, 104, 3, 19, 221, 168, 222, 83, 127,
+           203, 118, 126, 146, 47, 8, 190, 106, 223, 93, 102, 233, 2,
+           220, 192, 238, 155, 35, 161, 11, 130]
+        }
+        it 'returns the byte collection' do
+          expect(described_class.decode number).to eq byte_array
+        end
+      end
     end
   end
 end
