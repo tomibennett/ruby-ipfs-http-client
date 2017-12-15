59877f138fb7dfa278c4d44e4dc4be62f10b70de
tbenett <tom@benett.io>
2017-12-15 05:59:21 +0100

    refactor(file): a multihash can be used to construct an ipfs file

--- lib/file.rb
+++ lib/file.rb
@@ -3,20 +3,17 @@ require_relative './api/files/add'
 
 module Ipfs
   class File
-    attr_reader :multihash, :size, :name
+    attr_reader :path, :multihash, :size, :name
 
-    def initialize(argument)
-      if ::File.file? argument
-        @filepath = argument
-        @content = ::File.read(@filepath)
-      else
-        raise Errno::ENOENT, 'no such file or directory'
-      end
+    def initialize(**attributes)
+      attributes.each { |name, value|
+        instance_variable_set("@#{name}".to_sym, send("init_#{name}", value))
+      }
     end
 
     def add
       tap {
-        Ipfs::Client.execute(Command::Add, @filepath).tap { |response|
+        Ipfs::Client.execute(Command::Add, @path).tap { |response|
           @multihash = Multihash.new response['Hash']
           @size = response['Size'].to_i
           @name = response['Name']
@@ -25,13 +22,27 @@ module Ipfs
     end
 
     def cat
-      @content
+      @path ? ::File.read(@path) : ''
     end
 
     private
 
     def added?
-      @multihash != nil
+      !@multihash.nil?
+    end
+
+    def init_multihash(multihash)
+      multihash.is_a?(String) \
+        ? Multihash.new(multihash) \
+        : multihash
+    end
+
+    def init_path(path)
+      if ::File.file? path
+        path
+      else
+        raise Errno::ENOENT, 'no such file or directory'
+      end
     end
   end
 end
--- spec/file_spec.rb
+++ spec/file_spec.rb
@@ -1,33 +1,80 @@
 require_relative '../lib/file'
 
 RSpec.describe Ipfs::File do
-  let(:file_path) { ::File.join('spec', 'fixtures', 'dune.txt') }
-  let(:file) { described_class.new file_path }
+  let(:path) { ::File.join('spec', 'fixtures', 'dune.txt') }
 
   describe '.initialize' do
     context 'given a file path' do
-      it 'can initialize from a filepath if the file exists' do
-        expect(described_class.new file_path).to be_a described_class
+      it 'can creates an Ipfs file from a filepath if the file exists' do
+        expect(described_class.new path: path).to be_a described_class
       end
 
       it 'raises an error if the path given does not lead to a file' do
-        expect { described_class.new '' }.to raise_error Errno::ENOENT
+        expect { described_class.new path: '' }.to raise_error Errno::ENOENT
       end
 
-      it 'has not metadata associated with the file given by Ipfs' do
+      it 'has no metadata associated with the file given by Ipfs' do
+        file = described_class.new path: path
+
         expect(file.name).to be_nil
         expect(file.size).to be_nil
         expect(file.multihash).to be_nil
+      end
+    end
+
+    context 'given a multihash' do
+      context 'multihash is a string' do
+        let(:multihash) { 'QmcUewL8t3B4aW1VTwbLLgxQLi4hMFrb1ASwLsM1uebSs5' }
+        let(:file) { described_class.new multihash: multihash }
+
+        it 'can create an Ipfs file if the multihash is valid' do
+          expect(file).to be_a described_class
+        end
+
+        it 'cannot create the file if the multihash is invalid' do
+          expect{
+            described_class.new multihash: ''
+          }.to raise_error Ipfs::Error::InvalidMultihash
+        end
+
+        it 'has the multihash set' do
+          expect(file.multihash).to be_a Ipfs::Multihash
+        end
+
+        it 'size and name are not set' do
+          expect(file.name).to be_nil
+          expect(file.size).to be_nil
+        end
+      end
+
+      context 'multihash is an object' do
+        let(:multihash) { double('Ipfs::Multihash') }
+        let(:file) { described_class.new multihash: multihash}
+
+        it 'can create an Ipfs file' do
+          expect(file).to be_a described_class
+        end
+
+        it 'has the multihash set' do
+          expect(file.multihash).to eq multihash
+        end
+
+        it 'size and name are not set' do
+          expect(file.name).to be_nil
+          expect(file.size).to be_nil
+        end
       end
     end
   end
 
   describe '#add' do
     context 'the file was never added to Ipfs' do
+      let(:file) { described_class.new path: path }
+
       before do
         expect(Ipfs::Client)
           .to receive(:execute)
-                .with(Ipfs::Command::Add, file_path)
+                .with(Ipfs::Command::Add, path)
                 .and_call_original
       end
 
@@ -38,19 +85,21 @@ RSpec.describe Ipfs::File do
       it 'has metadata associated with the file given by Ipfs' do
         file.add
 
-        expect(file.name).to eq ::File.basename(file_path)
+        expect(file.name).to eq ::File.basename(path)
         expect(file.size).to be_an Integer
         expect(file.multihash).to be_a Ipfs::Multihash
       end
     end
 
     context 'the file has already been added to Ipfs' do
+      let(:file) { described_class.new path: path }
+
       before do
         file.add
 
         expect(Ipfs::Client)
           .to_not receive(:execute)
-                    .with(Ipfs::Command::Add, file_path)
+                    .with(Ipfs::Command::Add, path)
                     .and_call_original
       end
 
@@ -59,7 +108,7 @@ RSpec.describe Ipfs::File do
       end
 
       it 'has metadata associated with the file given by Ipfs' do
-        expect(file.name).to eq ::File.basename(file_path)
+        expect(file.name).to eq ::File.basename(path)
         expect(file.size).to be_an Integer
         expect(file.multihash).to be_a Ipfs::Multihash
       end
@@ -68,8 +117,10 @@ RSpec.describe Ipfs::File do
 
   describe '#cat' do
     context 'a file was passed to instantiate the object' do
+      let(:file) { described_class.new path: path }
+
       it 'returns the content of the file' do
-        expect(file.cat).to eq ::File.read file_path
+        expect(file.cat).to eq ::File.read path
       end
 
       it 'does not call the client' do
