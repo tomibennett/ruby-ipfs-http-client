07cf86e80c14fba3ecfb5989b0fe652ed7a2f583
tbenett <tom@benett.io>
2018-01-22 12:16:12 +0100

    feat(doc): Ipfs::File#add

--- lib/file.rb
+++ lib/file.rb
@@ -35,6 +35,37 @@ module Ipfs
       @added = false
     end
 
+    # Add a file to the Ipfs' node.
+    #
+    # @note the call to Ipfs completes data about the added file.
+    #   See {#multihash}, {#size} and {#name}.
+    #
+    # An {#Ipfs::File} instantiated from a multihash will not be added to Ipfs
+    # (as the presence of the multihash already suppose its addition to a node).
+    # In such case, the object is still returned but no call to Ipfs occurs.
+    #
+    # @example file not being added to Ipfs
+    #   file = Ipfs::File.new(path: 'path/to/file')
+    #   file.cat
+    #   #=> ''
+    #   file.multihash
+    #   #=> nil
+    #   file.name
+    #   #=> nil
+    #   file.size
+    #   #=> nil
+    # @example file being added
+    #  file = Ipfs::File.new(path: 'path/to/file').add
+    #  file.cat
+    #  #=> 'file content'
+    #  file.multihash
+    #  #=> #<Ipfs::Multihash ...>
+    #  file.name
+    #  #=> 'file'
+    #  file.size
+    #  #=> 20
+    #
+    # @return [Ipfs::File] Returns the object on which the method was call.
     def add
       tap {
         Ipfs::Client.execute(Command::Add, @path).tap { |response|
