5dc7fac699dc79c26d764bdb447368d3810d15c9
tbenett <tom@benett.io>
2019-02-18 21:33:03 +0100

    feat(doc): Ipfs::Client.id

--- lib/client.rb
+++ lib/client.rb
@@ -41,6 +41,25 @@ module Ipfs
         command.parse_response call command.build_request *args
       end
 
+      # Various debugging information concerning the Ipfs node itself
+      #
+      # @example
+      #   Ipfs::Client.id
+      #   #=> {
+      #     peer_id: 'QmVnLbr9Jktjwx...',
+      #     addresses: [
+      #       "/ip4/127.0.0.1/tcp/4001/ipfs/QmVwxnW4Z8JVMDfo1jeFMNqQor5naiStUPooCdf2Yu23Gi",
+      #       "/ip4/192.168.1.16/tcp/4001/ipfs/QmVwxnW4Z8JVMDfo1jeFMNqQor5naiStUPooCdf2Yu23Gi",
+      #       "/ip6/::1/tcp/4001/ipfs/QmVwxnW4Z8JVMDfo1jeFMNqQor5naiStUPooCdf2Yu23Gi",
+      #       "/ip6/2a01:e34:ef8d:2940:8f7:c616:...5naiStUPooCdf2Yu23Gi",
+      #       "/ip6/2a01:e34:ef8d:2940:...5naiStUPooCdf2Yu23Gi",
+      #       "/ip4/78.248.210.148/tcp/13684/ipfs/Qm...o1jeFMNqQor5naiStUPooCdf2Yu23Gi"
+      #     ],
+      #     public_key: "CAASpgIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwgg...AgMBAAE=",
+      #     agent_version: "go-ipfs/0.4.13/3b16b74"
+      #   }
+      #
+      # @return [Hash{Symbol => String, Array<String>}]
       def id
         @@id
       end
